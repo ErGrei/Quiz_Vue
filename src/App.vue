79f8faf3c3858c73b24919342fec6c6661001031
ErGrei <i.s.piskunov@gmail.com>
2024-09-01 12:57:56 +0400

    feat: add components card

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import quizes from "./data/quizes.json";
 import { ref } from "vue";
+import Card from "./components/quizStartCard.vue";
 
 const quizesRef = ref(quizes);
 </script>
@@ -12,16 +13,7 @@ const quizesRef = ref(quizes);
       <input type="text" placeholder="Search..." />
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizesRef"  :key="quiz.id" class="card">
-        <img
-          src="./assets/01_fishing _line.jpg"
-          alt="Абстракный человечек задумывается на фоне красного вопроса"
-        />
-        <div class="card-text">
-          <h2>{{quiz.name}}</h2>
-          <p>{{quiz.questions.length}} вопросов</p>
-        </div>
-      </div>
+     <Card v-for="quiz in quizesRef" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
@@ -66,33 +58,5 @@ header input {
   gap: 20px;
 }
 
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 15px #8de9e1;
-  margin: 0, auto;
-  cursor: pointer;
-  transition: all 0.3s ease;
-}
 
-.card:hover {
-  transform: scale(1.1);
-  box-shadow: 1px 1px 25px #e0b159;
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  border: 1px solid #cccccc;
-}
-
-.card .card-text {
-  padding: 0 5px;
-  text-align: center;
-}
-
-.card .card-text h2 {
-font-weight: bold;
-}
 </style>
---
+++ src/components/quizStartCard.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { defineProps } from "vue";
+
+const { quiz } = defineProps(["quiz"]);
+
+
+console.log(quiz);
+</script>
+
+<template>
+  <div class="card">
+    <img
+      :src="quiz.img"
+      alt="Абстракный человечек задумывается на фоне красного вопроса"
+    />
+    <div class="card-text">
+      <h2>{{ quiz.name }}</h2>
+      <p>{{ quiz.questions.length }} вопросов</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 15px #8de9e1;
+  margin: 0, auto;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+.card:hover {
+  transform: scale(1.1);
+  box-shadow: 1px 1px 25px #e0b159;
+}
+
+.card img {
+  width: 100%;
+  height: 190px;
+  border: 1px solid #cccccc;
+}
+
+.card .card-text {
+  padding: 0 5px;
+  text-align: center;
+}
+
+.card .card-text h2 {
+  font-weight: bold;
+}
+</style>
